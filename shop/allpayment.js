3b9021ca69f1cb0f53944160bfd910f1de069fe6
AlphaOfc <https://github.com/AlphaOfc/alphagood.git>
2021-12-07 20:13:03 +0700

    alphagood

---
+++ shop/allpayment.js
@@ -0,0 +1,16 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ALPHA BOT
+
+1. DANA
+	ㅁ 0822 3256 8963
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
